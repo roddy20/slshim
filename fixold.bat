2cd3cdb54037ff712c539dae40894c3d5c1c17e5
Rodion Shingarev <rodion@Mac-Pro-Rodion.local>
2018-11-03 23:40:33 +0200

    slshim-mod

--- fixold.bat
+++
@@ -1,3 +0,0 @@
-reg add HKLM\SYSTEM\CurrentControlSet\services\sppuinotify /f /v Type /t REG_DWORD /d 32
-reg add HKLM\SYSTEM\CurrentControlSet\services\sppsvc /f /v Type /t REG_DWORD /d 16
-reg add HKLM\SYSTEM\CurrentControlSet\services\osppsvc /f /v Type /t REG_DWORD /d 16
